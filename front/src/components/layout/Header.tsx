3d47ac0726c005f7645830baf4d65f4ac5f71073
Arvinbuy7n <arwinbuyan88705548@gmail.com>
2024-03-11 18:41:31 +0800

    header

--- front/src/components/layout/Header.tsx
+++ front/src/components/layout/Header.tsx
@@ -1,15 +1,15 @@
 export const Header = () => {
   return (
     <div className="flex bg-black w-full justify-center">
-      <div className="justify-between flex px-6 py-4 w-[1700px]">
-        <img src="pinelogo.png" alt="" width={36} />
+      <div className="justify-between flex px-6 py-2 w-[1600px]">
+        <img src="pinelogo.png" className="w-[36px] py-2" />
 
-        <div className="flex gap-8">
-          <img src="bell.png" alt="" width={32} />
+        <div className="flex gap-6">
+          <img src="bell.png" className="py-3 w-[28px]" />
 
-          <div className="flex gap-4">
-            <img src="container.png"></img>
-            <p className="text-white text-[14px] font-normal pt-2">Username </p>
+          <div className="flex gap-5">
+            <img src="container.png" className="w-[46px]" />
+            <p className="text-white text-[16px] font-normal pt-4">Username</p>
           </div>
         </div>
       </div>
