5f53ca3b2c440f03de566fbe6de11441213d51ee
Arvinbuyan Otgonjargal <23LP6614@G301-CC0207.local>
2024-03-11 10:41:40 +0800

    control head

--- front/src/components/admin/AdminControlHead.tsx
+++ front/src/components/admin/AdminControlHead.tsx
@@ -2,9 +2,9 @@ export const AdminControlHead = () => {
   return (
     <div className="flex w-full gap-6 px-8 py-10">
       <div className="basis-0 grow">
-        <div className="py-4 px-8 bg-white rounded-lg gap-3 flex flex-col">
+        <div className="py-4 px-8 bg-white rounded-lg gap-4 flex flex-col">
           <div className="flex gap-3">
-            <img src="dollar.png" />
+            <img src="dollar.png" className="py-1" />
             <p className="text-base font-semibold font-inter">Орлого</p>
           </div>
 
@@ -13,10 +13,11 @@ export const AdminControlHead = () => {
           <p className="text-sm text-[#5E6166] font-normal">Өнөөдөр</p>
         </div>
       </div>
+
       <div className="basis-0 grow">
-        <div className="py-4 px-8 bg-white rounded-lg gap-3 flex flex-col">
+        <div className="py-4 px-8 bg-white rounded-lg gap-4 flex flex-col">
           <div className="flex gap-3">
-            <img src="bag.png" />
+            <img src="bag.png" className="" />
             <p className="text-base font-semibold font-inter">Захиалга</p>
           </div>
 
@@ -25,8 +26,9 @@ export const AdminControlHead = () => {
           <p className="text-sm text-[#5E6166] font-normal">Өнөөдөр</p>
         </div>
       </div>
+
       <div className="basis-0 grow">
-        <div className="py-4 px-8 bg-white rounded-lg gap-3 flex flex-col">
+        <div className="py-4 px-8 bg-white rounded-lg gap-4 flex flex-col">
           <div className="flex gap-3">
             <img src="bag.png" />
             <p className="text-base font-semibold font-inter">Хэрэглэгч</p>
