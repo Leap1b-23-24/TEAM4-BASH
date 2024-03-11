b057b1e7184810b291281d3d04f1750ec274bdbd
Sarangoo Tsogtbaatar <23LP8796@G301-CC0189.local>
2024-03-11 12:25:46 +0800

    Dashboard last

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,21 +1,12 @@
 import { AdminDashboard } from "../components/admin/AdminDashboard";
-<<<<<<< HEAD
-import Footer from "../components/footer";
-=======
 import { AdminTable } from "../components/admin/AdminTable";
 import { AdminTableGeneral } from "../components/admin/AdminTableGeneral";
->>>>>>> 0b382b1 (03.07 admin dashboard table and charts)
 import { Header } from "../components/layout/Header";
 
 export default function Home() {
   return (
     <div className="w-full h-screen max-w-[1700px] m-auto">
       <AdminDashboard />
-<<<<<<< HEAD
-      <Footer></Footer>
-=======
-      <AdminTableGeneral />
->>>>>>> 0b382b1 (03.07 admin dashboard table and charts)
     </div>
   );
 }
--- front/src/components/admin/AdminTableGeneral.tsx
+++ front/src/components/admin/AdminTableGeneral.tsx
@@ -3,7 +3,7 @@ import { AdminTable } from "./AdminTable";
 
 export const AdminTableGeneral = () => {
   return (
-    <div className="flex gap-[30px]">
+    <div className="flex gap-[30px] px-8">
       <AdminTable />
       <AdminChart />
     </div>
