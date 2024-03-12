bfd10215f16514ee061ea434df77403617ba2283
Arvinbuy7n <arwinbuyan88705548@gmail.com>
2024-03-12 18:16:04 +0800

    sign

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,3 +1,9 @@
+import { SignUp } from "../components/auto/SignUp";
+
 export default function Home() {
-  return <div className="w-full h-screen max-w-[1700px] m-auto bg-white"></div>;
+  return (
+    <div className="w-full h-screen max-w-[1700px] m-auto bg-white">
+      <SignUp />
+    </div>
+  );
 }
