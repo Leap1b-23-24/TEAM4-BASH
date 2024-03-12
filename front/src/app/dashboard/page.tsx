bd08ed1cec528cce654fcd937c731e1e56b3f238
Arvinbuyan Otgonjargal <23LP6614@G301-CC0207.local>
2024-03-12 11:40:35 +0800

    dashboard

---
+++ front/src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { AdminControlHead } from "@/src/components/admin/AdminControlHead";
+import { AdminTableGeneral } from "@/src/components/admin/AdminTableGeneral";
+
+export default function Home() {
+  return (
+    <div className="w-full h-full bg-[#F7F7F8]">
+      <AdminControlHead />
+      <AdminTableGeneral />
+    </div>
+  );
+}
--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,9 +1,3 @@
-import { ControlHead } from "../components/admin/ControlHead";
-
 export default function Home() {
-  return (
-    <div className="w-full h-screen max-w-[1700px] m-auto">
-      <ControlHead />
-    </div>
-  );
+  return <div className="w-full h-screen max-w-[1700px] m-auto bg-white"></div>;
 }
