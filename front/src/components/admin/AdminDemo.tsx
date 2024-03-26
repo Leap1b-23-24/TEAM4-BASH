c14e8ecbd368bd9b3a5c9be5f599c54b8d673a02
Handmaa Bayanmunkh <handmaabayanmunkh@Handmaas-MacBook-Pro.local>
2024-03-26 11:15:20 +0800

    init

--- front/src/components/admin/AdminDemo.tsx
+++ front/src/components/admin/AdminDemo.tsx
@@ -24,7 +24,7 @@ export const AdminDemo = () => {
 
   return (
     <div className="w-[full] h-[full] bg-[white]  ">
-      <div className="bg-[#7E33E0] py-4">
+      <div className="bg-[#7E33E0] w-[full] py-4">
         <Container sx={{ display: "flex", justifyContent: "space-between" }}>
           <div className="flex gap-20">
             <div className="flex gap-2">
@@ -84,10 +84,10 @@ export const AdminDemo = () => {
             </div>
           </div>
 
-          <div className="flex">
+          <div className="flex w-[full]">
             <input
               type="text"
-              className="border-2 border-[#E7E6EF] width-[315px] h-8"
+              className="border-2 border-[#E7E6EF] width-[full] h-8"
             />
             <div>
               <Search
@@ -111,7 +111,7 @@ export const AdminDemo = () => {
             alignItems: "center",
           }}
         >
-          <div className=" ">
+          <div className="w-[full] ">
             <div className="flex flex-row gap-1">
               <p className="text-base">Home</p>
               <p className=" text-base text-[#FB2E86]">. Order Complete</p>
@@ -119,7 +119,7 @@ export const AdminDemo = () => {
           </div>
         </Container>
       </div>
-      <div className="bg-[white]  py-20 ">
+      <div className="bg-[white] w-[full]  py-40  ">
         <Container
           sx={{
             display: "flex",
@@ -127,24 +127,24 @@ export const AdminDemo = () => {
             alignItems: "center",
           }}
         >
-          <div className="w-[1200px] h-[999px] flex flex-col gap-8 ">
+          <div className="w-[full] h-[full] flex flex-col gap-8 ">
             <p className="text-[24px] text-[#1D3178]">Hekto Demo</p>
             <p className="text-[12px] text-[#1D3178]">
               Cart/ Information/ Shipping/ Payment
             </p>
-            <div className="flex flex-row gap-8">
-              <div className=" flex flex-col gap-20 w-[792px] h-[920px]  rounded items-center justify-center bg-[#F8F8FD] ">
-                <div className="flex flex-col gap-10">
+            <div className="flex flex-row gap-8 w-[full]">
+              <div className=" flex flex-col gap-20 w-[70%] h-[full] rounded px-10 justify-center bg-[#F8F8FD]  ">
+                <div className="flex flex-col w-[full] gap-10">
                   <p className="text-[#1D3178] text-[18px] font-bold">
                     Contact Information
                   </p>
 
-                  <div className=" flex flex-col gap-6">
+                  <div className=" flex flex-col w-[full] gap-6">
                     <TextField
                       id="standard"
                       label="Email or mobile phone number"
                       variant="standard"
-                      sx={{ width: "708px", color: "#F8F8FD" }}
+                      sx={{ width: "full", color: "#F8F8FD" }}
                     />
                     <FormControlLabel
                       control={<Checkbox />}
@@ -153,67 +153,67 @@ export const AdminDemo = () => {
                     />
                   </div>
                 </div>
-                <div className="flex flex-col gap-10">
+                <div className="flex flex-col w-[full] gap-10">
                   <p className="text-[#1D3178] text-[18px] font-bold">
                     Shipping address
                   </p>
-                  <div className="flex flex-col gap-6">
-                    <div className="flex flex-row gap-8">
+                  <div className="flex flex-col w-[full] gap-6">
+                    <div className="flex flex-row  w-[full] gap-5">
                       <TextField
                         id="standard"
                         label="First name (optional)"
                         variant="standard"
-                        sx={{ width: "336px", color: "#F8F8FD" }}
+                        sx={{ width: "50%", color: "#F8F8FD" }}
                       />
                       <TextField
                         id="standard"
                         label="Last name"
                         variant="standard"
-                        sx={{ width: "336px", color: "#F8F8FD" }}
+                        sx={{ width: "50%", color: "#F8F8FD" }}
                       />
                     </div>
                     <TextField
                       id="standard"
                       label="Address"
                       variant="standard"
-                      sx={{ width: "708px", color: "#F8F8FD" }}
+                      sx={{ width: "full", color: "#F8F8FD" }}
                     />
                     <TextField
                       id="standard"
                       label="Appaetnentment,suit,e.t.c (optinal)"
                       variant="standard"
-                      sx={{ width: "708px", color: "#F8F8FD" }}
+                      sx={{ width: "full", color: "#F8F8FD" }}
                     />
                     <TextField
                       id="standard"
                       label="City"
                       variant="standard"
-                      sx={{ width: "708px", color: "#F8F8FD" }}
+                      sx={{ width: "full", color: "#F8F8FD" }}
                     />
                     <div className="flex flex-row gap-8">
                       <TextField
                         id="Bangladesh"
                         label="First name (optional)"
                         variant="standard"
-                        sx={{ width: "336px", color: "#F8F8FD" }}
+                        sx={{ width: "50%", color: "#F8F8FD" }}
                       />
                       <TextField
                         id="standard"
                         label="Postal Code"
                         variant="standard"
-                        sx={{ width: "336px", color: "#F8F8FD" }}
+                        sx={{ width: "50%", color: "#F8F8FD" }}
                       />
                     </div>
                   </div>
                 </div>
-                <div className="flex flex-col justify-start w-[708px]">
-                  <button className="w-[182px] h-[44px] rounded bg-[#FB2E86] text-[white] text-[16px]">
+                <div className="flex flex-col justify-start w-[30%] ">
+                  <button className="w-[full] h-[44px] rounded bg-[#FB2E86] text-[white] px-4 text-[16px]">
                     Continue Shipping
                   </button>
                 </div>
               </div>
-              <div className="flex flex-col gap-10">
-                <div className="h-[600px]  w-[385px] flex flex-col overflow-scroll relative overflow-hidden gap-5	">
+              <div className="flex flex-col w-[40%] gap-10">
+                <div className="h-[600px]  w-[full] flex flex-col overflow-scroll relative overflow-hidden gap-5	">
                   {productList.map((item, index) => {
                     return (
                       <AdminScroll
@@ -226,10 +226,12 @@ export const AdminDemo = () => {
                     );
                   })}
                 </div>
-                <div className="w-[100%] h-[243px] border bg-[#F4F4FC] flex flex-col justify-center gap-5 px-8 ">
-                  <div className="border-b-[2px] text-[18px]  flex flex-row gap-40 py-2 ">
-                    <p className="text-[18px] text-[#1D3178]">Нийлбэр:</p>
-                    <p className="text-[18px] font-bold text-[#151875] ">
+                <div className="w-[full] h-[243px] border bg-[#F4F4FC] flex flex-col justify-center gap-5 px-8 ">
+                  <div className="border-b-[2px] text-[18px]  flex flex-row  py-2 ">
+                    <p className="text-[18px] text-[#1D3178] w-[70%] px-2">
+                      Нийлбэр:
+                    </p>
+                    <p className="text-[18px] font-bold text-[#151875] w-[20%] px-2 ">
                       {productList.reduce(
                         (currentValue, total) =>
                           currentValue + Number(total.mainPrice),
@@ -238,9 +240,11 @@ export const AdminDemo = () => {
                       ₮
                     </p>
                   </div>
-                  <div className="border-b-[2px] text-[18px]  text-[#151875] flex flex-row gap-[140px] py-2  ">
-                    <p className="text-[18px] text-[#1D3178]">Төлөх дүн:</p>
-                    <p className="text-[20px] font-bold text-[#151875]">
+                  <div className="border-b-[2px] text-[18px]  text-[#151875] flex flex-row  py-2  ">
+                    <p className="text-[18px] text-[#1D3178] w-[70%] px-2">
+                      Төлөх дүн:
+                    </p>
+                    <p className="text-[20px] font-bold text-[#151875] w-[20%] px-1">
                       {productList.reduce(
                         (currentValue, total) =>
                           currentValue + Number(total.mainPrice),
@@ -249,7 +253,7 @@ export const AdminDemo = () => {
                       ₮
                     </p>
                   </div>
-                  <button className="bg-[#19D16F] w-[323px] h-[40px] text-[white] rounded ">
+                  <button className="bg-[#19D16F] w-[full] h-[40px] text-[white] rounded ">
                     Худалдан авах
                   </button>
                 </div>
